09c098f345ba148e8c475d409cf93e7f437f1cbf
ashsei <58061442+ashsei@users.noreply.github.com>
2020-07-08 20:37:43 -0600

    Another Crack at Controller Bug, Not Resolved - Pick Up Here Tomorrow

--- controllers/properties.js
+++ controllers/properties.js
@@ -4,19 +4,19 @@ const Property = require('../models/propertySchema.js')
 
 // ROUTES //
     // NEW //
-    router.get('/vendom/add', (req, res) => {
+    router.get('/add', (req, res) => {
         res.render('newProperty.ejs', {
             tabTitle: 'Add Property',
         });
     });
     // CREATE //
-    router.post('/vendom', (req, res) => {
+    router.post('/', (req, res) => {
         Property.create(req.body, (error, createdProperty) => {
             res.redirect('/vendom');
         });
     });
     // SHOW //
-    router.get('/vendom/:id', (req, res) => {
+    router.get('/:id', (req, res) => {
         Property.findById(req.params.id, (error, foundProperty) => {
             res.render('propertyShow.ejs', {
                 property: foundProperty,
@@ -25,7 +25,7 @@ const Property = require('../models/propertySchema.js')
         });
     });
     // INDEX //
-    router.get('/vendom', (req, res) => {
+    router.get('/', (req, res) => {
         Property.find({}, (error, allProperties) => {
             res.render('mainIndex.ejs', {
                 properties: allProperties,
@@ -34,13 +34,13 @@ const Property = require('../models/propertySchema.js')
         });
     });
     // DELETE //
-    router.delete('/vendom/:id', (req, res) => {
+    router.delete('/:id', (req, res) => {
         Property.findByIdAndRemove(req.params.id, (error, data) => {
             res.redirect('/vendom');
         });
     });
     // EDIT //
-    router.get('/vendom/:id/edit', (req, res) => {
+    router.get('/:id/edit', (req, res) => {
         Property.findById(req.params.id, (error, foundProperty)=> {
             res.render(
                 'editProperty.ejs',
@@ -52,7 +52,7 @@ const Property = require('../models/propertySchema.js')
         });
     });
     // PUT //
-    router.put('/vendom/:id', (req, res) => {
+    router.put('/:id', (req, res) => {
         Property.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedModel) => {
             res.redirect('/vendom');
         });
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = 3000;
 const propertiesController = require('./controllers/properties.js');
-app.use(propertiesController);
+app.use('/vendom', propertiesController);
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}));
