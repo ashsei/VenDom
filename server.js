93ffa438c7d82c2e7005a68012a7598c3e3e59f0
ashsei <58061442+ashsei@users.noreply.github.com>
2020-07-09 20:47:16 -0600

    Server Updated in Preperation for Deployment to Heroku

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-// SETUP //
+// DEPENDENCIES //
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const app = express();
-const PORT = 3000;
 const propertiesController = require('./controllers/properties.js');
+const db = mongoose.connection;
 
+// PORT //
+const PORT = process.env.PORT || 3000;
+
+// DATABASE //
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `vendom`;
+
+mongoose.connect(MONGODB_URI , { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+    useFindAndModify: false });
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+db.on('open' , ()=>{});
+
+// MIDDLEWEAR //
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
